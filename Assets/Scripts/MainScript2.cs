b836c7412f2b934e0525cec32b8c17d3b64a4a9f
JackBaker1 <163625@chrome.escg.ac.uk>
2021-10-14 22:08:46 +0100

    made the wheel

---
+++ Assets/Scripts/MainScript2.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainScript2: MonoBehaviour
+{
+
+    public float Speed = 600;
+    public bool isSpin = true;
+
+    public GameObject pointer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RotateWheel();
+    }
+
+
+    void RotateWheel()
+    {
+        transform.Rotate(0, 0, Speed * Time.deltaTime);
+
+        if (isSpin == false && Speed > 0)
+        {
+            Stop();
+        }
+    }
+
+    public void Stop()
+    {
+
+        Speed--;
+        if (Speed <= 10)
+        {
+            pointer.GetComponent<BoxCollider2D>().enabled = true;
+        }
+
+
+        else if (Speed <= 0)
+        {
+            
+          Speed = 0;
+        }
+    }
+
+}
